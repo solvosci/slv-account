2774327f202f0801212b9cf880d44338b04661c4
David Alonso // Solvos <david.alonso@solvos.es>
2021-03-12 14:02:03 +0100

    [ADD] purchase_account_invoice_report_grouped_by_picking_extended

---
+++ purchase_account_invoice_report_grouped_by_picking_extended/README.rst
@@ -0,0 +1,19 @@
+=========================================================
+Account Invoice Grouped by Picking - extended to Purchase
+=========================================================
+
+Print invoice lines grouped by picking, including purchase invoices
+and taking into account internal pickings
+
+Credits
+=======
+
+Authors
+~~~~~~~
+
+* Solvos
+
+Contributors
+~~~~~~~~~~~~
+
+* David Alonso <david.alonso@solvos.es>
---
+++ purchase_account_invoice_report_grouped_by_picking_extended/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Account Invoice Grouped by Picking - extended to Purchase",
+    "summary": """
+        Print invoice lines grouped by picking, including purchase invoices
+        and taking into account internal pickings
+    """,
+    "version": "13.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://github.com/solvosci/slv-account",
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "depends": [
+        "purchase_account_invoice_report_grouped_by_picking",
+        "purchase_stock_picking_invoice_link_extended",
+    ],
+    "data": [],
+}
