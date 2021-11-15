86081a365bfb9b35939055655c587a03b5c86d44
ChristianSantamaria <christiancameselle@hotmail.com>
2021-11-15 12:50:36 +0100

    [ADD] account_move_pivot_view

---
+++ account_move_pivot_view/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Account Move Pivot View",
+    "summary": """
+        Adds pivot view to Invoices actions
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "Account",
+    "website": "https://github.com/solvosci/slv-account",
+    "depends": ["account"],
+    "data": [
+        "views/account_move_views.xml"
+    ],
+    "installable": True,
+}
