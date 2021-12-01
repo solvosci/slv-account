14d7d2eab4f7f45e3a2643da21c2eee1b2ee140f
David Alonso // Solvos <david.alonso@solvos.es>
2021-12-01 14:08:14 +0100

    [ADD] account_payment_term_base_date

---
+++ account_payment_term_base_date/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Account Payment Term Base Date",
+    "summary": """
+        Enables using a base date for payment computation.
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    'category': "Account",
+    "website": "https://github.com/solvosci/slv-account",
+    "depends": ["account"],
+    "data": [
+        "views/account_move_views.xml",
+    ],
+    'installable': True,
+}
---
+++ account_payment_term_base_date/readme/DESCRIPTION.rst
@@ -0,0 +1,5 @@
+Enables using a base date for payment computation.
+
+The new base date is initially set with Invoice Date, or today, as Odoo does.
+Once set, this date (that is always editable) will be used in due dates 
+calculations, instead of Invoice Date.
