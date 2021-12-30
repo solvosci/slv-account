f228a8294d9448b66224b8577625b757dae9242b
ChristianSantamaria <christiancameselle@hotmail.com>
2021-12-30 16:28:42 +0100

    [ADD] account_invoice_report_wo_taxation

---
+++ account_invoice_report_wo_taxation/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2021 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Account Invoice Report Without Taxation",
+    "summary": """
+        Hides line taxes data in account invoice report
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "14.0.1.0.0",
+    'category': "Accounting & Finance",
+    "website": "https://github.com/solvosci/slv-account",
+    "depends": ["account"],
+    "data": [
+        "reports/account_invoice_template.xml"
+    ],
+    'installable': True,
+}
