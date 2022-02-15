786f8a8ded3b712943cdb3f2565cfa767d2c217c
David Alonso // Solvos <david.alonso@solvos.es>
2022-02-15 18:12:34 +0100

    [ADD] account_move_accounting_date_tree

---
+++ account_move_accounting_date_tree/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2022 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Account Invoice - Accounting Date on tree view",
+    "summary": """
+        Shows Accounting Date on Invoices tree views
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.2.0.0",
+    'category': "Account",
+    "website": "https://github.com/solvosci/slv-account",
+    "depends": ["account_payment_term_extension"],
+    "data": [
+        "views/account_move_views.xml",
+    ],
+    'installable': True,
+}
