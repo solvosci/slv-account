efdda7f16616d303d85d5827e4e4856bef1dbeaa
David Alonso // Solvos <david.alonso@solvos.es>
2022-03-08 11:27:04 +0100

    [ADD] account_bank_statement_import_enable

---
+++ account_bank_statement_import_enable/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2022 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Account Bank Statement - Enable import",
+    "summary": """
+        Re-enable import option for bank statements, disable by default starting from v13
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    'category': "Account",
+    "website": "https://github.com/solvosci/slv-account",
+    "depends": ["account"],
+    "data": [
+        "views/account_assets.xml",
+    ],
+    'installable': True,
+}
---
+++ account_bank_statement_import_enable/readme/ROADMAP.rst
@@ -0,0 +1,2 @@
+This implementation removes JS file load for asset backend. Maybe a JS
+inheritance should be a less aggresive method.
