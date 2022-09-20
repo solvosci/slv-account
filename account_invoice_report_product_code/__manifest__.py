2c8065a8dac0b47d22db483bbe69186b506332b3
David Alonso // Solvos <david.alonso@solvos.es>
2022-09-20 14:07:54 +0200

    [ADD] account_invoice_report_product_code

---
+++ account_invoice_report_product_code/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2022 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Invoice Report - Add product code",
+    "summary": """
+        Adds new detail column with product code to invoice report
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "14.0.1.0.0",
+    "category": "Accounting/Accounting",
+    "website": "https://github.com/solvosci/slv-account",
+    "depends": ["account"],
+    "data": [
+        "report/account_move_templates.xml",
+    ],
+    'installable': True,
+}
