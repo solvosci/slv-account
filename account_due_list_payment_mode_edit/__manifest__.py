cab7a3e57eb68981cd3524e251b4c1e6a96c3d07
Omar (Comunitea) <omar@comunitea.com>
2022-10-02 14:42:55 +0200

    [MOV][REF] account_due_list_improvements -> account_due_list_payment_mode_edit

---
+++ account_due_list_payment_mode_edit/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Account Due List Payment mode editable",
+    "version": "13.0.1.0.0",
+    "author": "Comunitea",
+    "website": "https://www.comunitea.com",
+    "description": "Account Due List Improvements",
+    "depends": ['account_due_list_payment_mode'],
+    "data": ['views/account_move.xml'],
+    "installable": True,
+    'license': 'AGPL-3'
+}
