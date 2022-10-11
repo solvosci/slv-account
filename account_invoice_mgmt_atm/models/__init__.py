c6bcf1625a4cd4a141c81999f4f941fb8d2ddb52
Christian Santamaria <christian.santamaria@solvos.es>
2022-10-11 11:53:40 +0200

    [ADD] account_invoice_mgmt_atm

---
+++ account_invoice_mgmt_atm/models/__init__.py
@@ -0,0 +1,4 @@
+from . import purchase_order
+from . import account_payment
+from . import res_company
+from . import account_move
---
+++ account_invoice_mgmt_atm/readme/CONTRIBUTORS.rst
@@ -0,0 +1,2 @@
+* Adrian Resua <adrian.resua@solvos.es>
+* Christian Santamaría <christian.santamaria@solvos.es>
