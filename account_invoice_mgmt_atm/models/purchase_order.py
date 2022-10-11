dda461cf5d1e5712ca62dadcd51437bdbe7fcf12
Christian Santamaria <christian.santamaria@solvos.es>
2022-10-11 11:53:40 +0200

    [ADD] account_invoice_mgmt_atm

---
+++ account_invoice_mgmt_atm/models/purchase_order.py
@@ -0,0 +1,25 @@
+# © 2022 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
+
+from odoo import models, fields
+
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    invoice_count = fields.Integer(compute="_invoice_purchase_order_length")
+
+    def generate_invoice_purchase_report(self):
+        for line in self.invoice_ids.filtered(lambda x: x.state == 'posted' and x.atm_invoice):
+            return line.action_invoice_print()
+
+    def _invoice_purchase_order_length(self):
+        for record in self:
+            record.invoice_count = len(record.invoice_ids.filtered(lambda x: x.state == 'posted' and x.atm_invoice))
+
+    def create_atm_account_move(self):
+        result = super(PurchaseOrder, self).action_view_invoice()
+        result["context"]["default_atm_invoice"] = True
+        if self.amount_total < 1000:
+            result["context"]["default_invoice_cash_rounding_id"] = self.env.ref('account_invoice_mgmt_atm.account_cash_rounding_default').id
+        return result
---
+++ account_invoice_mgmt_atm/models/res_company.py
@@ -0,0 +1,11 @@
+# © 2022 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3.0 (https://www.gnu.org/licenses/lgpl-3.0.html)
+
+from odoo import models, fields
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    journal_ATM_id = fields.Many2one('account.journal', string='ATM journal')
+    partner_ATM_id = fields.Many2one('res.partner', string='ATM contact')
