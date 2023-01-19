c0943964797deaeb057fc08d2e739c2560885b79
LuPinheiro <gerallupinheiro@gmail.com>
2023-01-19 17:59:52 +0100

    [ADD] account_invoice_user_authorization_payment

---
+++ account_invoice_user_authorization_payment/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Account Invoice User Authorization Payment",
+    "summary": """
+        Account Invoice User Authorization Payment
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "15.0.1.0.0",
+    "category": "Account",
+    "website": "https://github.com/solvosci/slv-account",
+    "depends": [
+        "account"
+    ],
+    "data": [
+        "views/account_move_views.xml"
+    ],
+    'installable': True,
+}
---
+++ account_invoice_user_authorization_payment/readme/DESCRIPTION.rst
@@ -0,0 +1,4 @@
+Adds fields in account move:
+
+* Show and save the user responsible for making the payment of the invoice.
+* Show the user responsible for creating the invoice, add User Authorization Payment filter in group by.
