b500c143c1a81667cfd41d966fe12cef69e0e669
Christian Santamaria <christian.santamaria@solvos.es>
2023-02-02 11:28:49 +0100

    [ADD] purchase_line_refund_to_invoice_qty_force

---
+++ purchase_line_refund_to_invoice_qty_force/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2023 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Purchase Line Refund To Invoice Qty Force",
+    "summary": """
+        This module forces the quantity in the purchases order to never be subtracted.
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "14.0.1.0.0",
+    "category": "Purchases",
+    "website": "https://github.com/solvosci/slv-account",
+    "depends": ["purchase_line_refund_to_invoice_qty"],
+    "data": [
+        "views/account_move_views.xml",
+        "wizards/account_move_reversal_view.xml",
+    ],
+}
