c456bdd54a8fa1509507ecb0a37867475dfeeea7
Christian Santamaria <christian.santamaria@solvos.es>
2023-03-14 14:01:09 +0100

    [FIX] account_invoice_report_gbp_dmm: tweak quantity done function
    Code is adapted to receive data from purchase_account_invoice_report_grouped_by_picking,
    when installed, without need of manifest dependecy declaration.

--- account_invoice_report_gbp_dmm/models/account_move.py
+++ account_invoice_report_gbp_dmm/models/account_move.py
@@ -7,12 +7,22 @@ class AccountMove(models.Model):
     _inherit = "account.move"
 
     def _get_signed_quantity_done(self, invoice_line, move, sign):
+        qty = super()._get_signed_quantity_done(invoice_line, move, sign)
+        # Compatibility with solvosci/slv-account/purchase_account_invoice_report_grouped_by_picking
+        #  without need of addons strict dependency
+        end_locations = (
+            self.env.context.get("signed_quantity_done_endloc")
+            or
+            ["customer"]
+        )
+        qty_field = (
+            self.env.context.get("signed_quantity_done_qtyfield")
+            or
+            "qty_delivered_dest"
+        )
         if move.product_id.invoice_policy == "stock_move_dest":
-            qty = 0
-            if move.location_id.usage == "customer":
-                qty = -move.qty_delivered_dest * sign
-            elif move.location_dest_id.usage == "customer":
-                qty = move.qty_delivered_dest * sign
-        else:
-            qty = super()._get_signed_quantity_done(invoice_line, move, sign)
+            if move.location_id.usage in end_locations:
+                qty = -move[qty_field] * sign
+            elif move.location_dest_id.usage in end_locations:
+                qty = move[qty_field] * sign
         return qty
