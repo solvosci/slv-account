b8996b89d0704b354928939b35dfc116859f07e1
Christian Santamaria <christian.santamaria@solvos.es>
2023-03-14 14:01:09 +0100

    [FIX] account_invoice_report_gbp_dmm: tweak quantity done function
    Code is adapted to receive data from purchase_account_invoice_report_grouped_by_picking,
    when installed, without need of manifest dependecy declaration.

--- account_invoice_report_gbp_dmm/__manifest__.py
+++ account_invoice_report_gbp_dmm/__manifest__.py
@@ -8,7 +8,7 @@
     """,
     "author": "Solvos",
     "license": "AGPL-3",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     'category': "Account",
     "website": "https://github.com/solvosci/slv-account",
     "depends": [
