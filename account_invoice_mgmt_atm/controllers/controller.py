fba0267f0ead1f440361714c2ad50f60d6fe3625
Christian Santamaria <christian.santamaria@solvos.es>
2023-04-19 16:40:50 +0200

    [IMP] account_invoice_mgmt_atm: add ATM payment logs

--- account_invoice_mgmt_atm/controllers/controller.py
+++ account_invoice_mgmt_atm/controllers/controller.py
@@ -1,10 +1,12 @@
 # © 2022 Solvos Consultoría Informática (<http://www.solvos.es>)
 # License LGPL-3 - See https://www.gnu.org/licenses/lgpl-3.0.html
 
-from odoo import http
+from odoo import http, _
 from odoo.http import request
 import json
+import logging
 
+_logger = logging.getLogger(__name__)
 
 class AddonCajeroHttp(http.Controller):
 
@@ -54,58 +56,65 @@ class AddonCajeroHttp(http.Controller):
 
     @http.route('/cajero/EmisionCompletadaReciclaje', type='http', auth='api_key', methods=['GET', 'POST'], csrf=False)
     def cajero_emision_completada_reciclaje(self, *args, **post):
-        res_company_id = request.env["res.company"].search([])
-        ws_user = res_company_id.partner_ATM_id
-        json_data = request.httprequest.data
-        json_data = json.loads(json_data.decode('utf-8'))
-        #region
-        # POSTMAN
-        # {
-        #     "parCodigo":"BILL/2022/0009",
-        #     "parImporte":5,
-        #     "parCajero":0,
-        #     "parFormaPago":0,
-        #     "parNumeroCheque":121212,
-        #     "parIdPago":"BILL/2022/0009/13:10/15:10"
-        # }
-        #endregion
+        try:
+            res_company_id = request.env["res.company"].search([])
+            ws_user = res_company_id.partner_ATM_id
+            json_data = request.httprequest.data
+            json_data = json.loads(json_data.decode('utf-8'))
+            #region
+            # POSTMAN
+            # {
+            #     "parCodigo":"BILL/2022/0009",
+            #     "parImporte":5,
+            #     "parCajero":0,
+            #     "parFormaPago":0,
+            #     "parNumeroCheque":121212,
+            #     "parIdPago":"BILL/2022/0009/13:10/15:10"
+            # }
+            #endregion
 
-        account_invoice_id = request.env["account.move"].sudo().search([('encrypted_name_lower', '=', json_data["parCodigo"])])
-        amount = int(json_data["parImporte"])/100
-        atm_id = json_data["parCajero"] #NU
-        payment_type = json_data["parFormaPago"]
-        payment_ATM_id = json_data["parIdPago"]
-        check_number = json_data["parNumeroCheque"]
+            account_invoice_id = request.env["account.move"].sudo().search([('encrypted_name_lower', '=', json_data["parCodigo"])])
+            amount = int(json_data["parImporte"])/100
+            atm_id = json_data["parCajero"] #NU
+            payment_type = json_data["parFormaPago"]
+            payment_ATM_id = json_data["parIdPago"]
+            check_number = json_data["parNumeroCheque"]
 
-        vals = {}
+            vals = {}
 
-        if not request.env["account.payment"].sudo().search([('payment_ATM_id', '=', payment_ATM_id)]):
-            # if typeofMove == "inbound" or typeofMove == "internal_transfer" or typeofMove == "outbound": #Sell # Cash # Electronic Credit Card 1(Check Number) or 2(authorization number)
-            # if payment_type == 0 or payment_type == 2: # Cash # 2 pagos 1 ventas
-            #     payment_method_id = request.env["account.payment.method"].sudo().browse(2)
-            # elif payment_type == 1: # Checks
-            #     payment_method_id = request.env["account.payment.method"].sudo().browse(4)
-            check_number = {"payment_reference" : check_number}
-            #vals.update(check_number)
+            if not request.env["account.payment"].sudo().search([('payment_ATM_id', '=', payment_ATM_id)]):
+                # if typeofMove == "inbound" or typeofMove == "internal_transfer" or typeofMove == "outbound": #Sell # Cash # Electronic Credit Card 1(Check Number) or 2(authorization number)
+                # if payment_type == 0 or payment_type == 2: # Cash # 2 pagos 1 ventas
+                #     payment_method_id = request.env["account.payment.method"].sudo().browse(2)
+                # elif payment_type == 1: # Checks
+                #     payment_method_id = request.env["account.payment.method"].sudo().browse(4)
+                check_number = {"payment_reference" : check_number}
+                #vals.update(check_number)
 
-            typeofMove = "outbound"
-            vals.update({"partner_type": "customer"})
-            payment_method_id = res_company_id.sudo().journal_ATM_id.outbound_payment_method_ids[0]
-            vals.update({
-                "payment_type": typeofMove, #outbound
-                "payment_method_id": payment_method_id.id,
-                "amount": amount, #cantidad
-                "currency_id": res_company_id.currency_id.id, #outbound
-                "journal_id": res_company_id.journal_ATM_id.id, #Cash
-                "partner_id": res_company_id.partner_ATM_id.id, #Nicole
-                "communication": json_data["parCodigo"], #Da un poco igual
-                "payment_ATM_id": payment_ATM_id, # Codigo cajero
-                "invoice_ids": [(4, account_invoice_id.id)] # Facturas relacionadas
-            })
-            account_payment_temp = request.env["account.payment"].sudo().create(vals)
-            account_payment_temp.post()
-            return str({'retRegistro': True})
-        return str({'retRegistro': False})
+                typeofMove = "outbound"
+                vals.update({"partner_type": "customer"})
+                payment_method_id = res_company_id.sudo().journal_ATM_id.outbound_payment_method_ids[0]
+                vals.update({
+                    "payment_type": typeofMove, #outbound
+                    "payment_method_id": payment_method_id.id,
+                    "amount": amount, #cantidad
+                    "currency_id": res_company_id.currency_id.id, #outbound
+                    "journal_id": res_company_id.journal_ATM_id.id, #Cash
+                    "partner_id": res_company_id.partner_ATM_id.id, #Nicole
+                    "communication": json_data["parCodigo"], #Da un poco igual
+                    "payment_ATM_id": payment_ATM_id, # Codigo cajero
+                    "invoice_ids": [(4, account_invoice_id.id)] # Facturas relacionadas
+                })
+                account_payment_temp = request.env["account.payment"].sudo().create(vals)
+                account_payment_temp.post()
+                _logger.info(('ATM Payment successful: payment_id:%s invoice_id:%s quantity:%s') % (account_payment_temp.id, account_invoice_id.id, amount))
+                return str({'retRegistro': True})
+            else:
+                _logger.error(('ATM Payment failed alredy exist a payment_ATM_id: %s') % (payment_ATM_id))
+                return str({'retRegistro': False})
+        except Exception as e:
+            _logger.error(e)
+            return str({'retRegistro': False})
 
     @http.route('/cajero/PING', type='http', methods=['GET', 'POST'], auth='api_key', csrf=False)
     def cajero_ping(self, *args, **post):
