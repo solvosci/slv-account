a87a6ffcbd6f545f45b2b30711d51bc9aa153de6
Christian Santamaria <christian.santamaria@solvos.es>
2024-01-17 17:23:46 +0100

    [ADD] account_move_date_maturity

---
+++ account_move_date_maturity/__manifest__.py
@@ -0,0 +1,20 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Journal Item Due Date visible",
+    "summary": """
+        Makes Due Date visible for Journal Items
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "13.0.1.0.0",
+    "category": "Account",
+    "website": "https://github.com/solvosci/slv-account",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_move_views.xml",
+    ],
+    "installable": True,
+}
