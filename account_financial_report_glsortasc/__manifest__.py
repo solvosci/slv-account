cb4bf42159dbfc0bd476bbe790e2236b0d5d0d0d
David Alonso // Solvos <david.alonso@solvos.es>
2024-01-24 19:24:21 +0100

    [ADD] account_financial_report_glsortasc
    Ensures that move lines within General Ledger report are sorted
    by name ascendent if belongs to the same date.

---
+++ account_financial_report_glsortasc/__manifest__.py
@@ -0,0 +1,15 @@
+# © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License AGPL-3 - See https://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Account Financial Reports - General Ledger sort by name asc",
+    "summary": """
+        Ensures that move lines within General Ledger report are sorted
+        by name ascendent if belongs to the same date
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "15.0.1.0.0",
+    "category": "Accounting & Finance",
+    "website": "https://github.com/solvosci/slv-account",
+    "depends": ["account_financial_report"],
+}
---
+++ account_financial_report_glsortasc/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+Ensures that move lines within General Ledger report are sorted
+by name ascendent if belongs to the same date.
