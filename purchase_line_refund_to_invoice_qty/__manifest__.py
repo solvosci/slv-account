d3c8171ab1a75c541cab13a1fb667aa120ba9afc
Christian Santamaria <christian.santamaria@solvos.es>
2024-01-29 09:27:02 +0100

    [MIG] purchase_line_refund_to_invoice_qty: Migration to 13.0

--- purchase_line_refund_to_invoice_qty/__manifest__.py
+++ purchase_line_refund_to_invoice_qty/__manifest__.py
@@ -8,7 +8,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "14.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Purchases",
     "website": "https://github.com/solvosci/slv-account",
     "depends": ["purchase"],
