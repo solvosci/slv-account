ced4d2bd3df9b0e3d5a15232021c3094ab942650
Anxo82 <angel.rivas@solvos.es>
2024-03-05 12:17:03 +0100

    [IMP] account_move_promissory_note: add promissory note payment Date to supplier invoices

--- account_move_promissory_note/models/account_move.py
+++ account_move_promissory_note/models/account_move.py
@@ -9,8 +9,14 @@ class AccountMove(models.Model):
 
     payment_ids = fields.Many2many('account.payment', 'account_invoice_payment_rel', 'invoice_id' , 'payment_id')
     promissory_note_number = fields.Char(compute='_compute_promissory_note_number', store=True)
+    payment_date = fields.Date(compute='_compute_payment_date', store=True)
 
     @api.depends('payment_ids.promissory_note_number')
     def _compute_promissory_note_number(self):
         for record in self:
             record.promissory_note_number = ', '.join(record.payment_ids.filtered(lambda x: x.promissory_note_number).mapped('promissory_note_number'))
+    
+    @api.depends('payment_ids.payment_date')
+    def _compute_payment_date(self):
+        for record in self:
+            record.payment_date = max((payment.payment_date for payment in record.payment_ids if payment.payment_date), default=None)
