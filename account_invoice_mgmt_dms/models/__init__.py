2a1e90e88dd9252586d6a7f646c7d9c91b47263c
David Alonso // Solvos <david.alonso@solvos.es>
2024-03-26 16:07:23 +0100

    [IMP] account_invoice_mgmt_dms: enable key directory erase protection

--- account_invoice_mgmt_dms/models/__init__.py
+++ account_invoice_mgmt_dms/models/__init__.py
@@ -1,6 +1,7 @@
 from . import account_move
 from . import account_move_line
 from . import account_payment
+from . import dms_directory
 from . import dms_file
 from . import stock_move
 from . import purchase_order
