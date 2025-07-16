f026fe4fdd7e78fbc43cc67ff9d063b383497153
Iria Alonso <iria.alonso@solvos.es>
2025-07-16 12:28:46 +0200

    [ADD] account_invoice_report_refund_total Show on report refund invoices total summary if it is a refund

---
+++ account_invoice_report_refund_total/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Account Invoice Report Refund Total",
+    "summary": """
+        Show on report refund invoices total summary if it is a refund
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "17.0.1.0.0",
+    'category': "Accounting & Finance",
+    "website": "https://github.com/solvosci/slv-account",
+    "depends": ["account"],
+    "data": [
+        "reports/account_invoice_template.xml"
+    ],
+    'installable': True,
+}
