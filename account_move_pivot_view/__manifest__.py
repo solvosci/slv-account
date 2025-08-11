bf25d5a0997277ddf87a998fb2090100fe931e20
Laura Pérez // Solvos <laura.perez@solvos.es>
2025-08-11 11:22:55 +0200

    [MIG] account_move_pivot_view: Migration to 17.0

--- account_move_pivot_view/__manifest__.py
+++ account_move_pivot_view/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "13.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Account",
     "website": "https://github.com/solvosci/slv-account",
     "depends": ["account"],
@@ -15,4 +15,5 @@
         "views/account_move_views.xml"
     ],
     "installable": True,
+    "uninstall_hook": "uninstall_hook",
 }
---
+++ account_move_pivot_view/hooks.py
@@ -0,0 +1,19 @@
+# © 2025 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+
+
+def uninstall_hook(env):
+
+    actions_to_restore = [
+        'account.action_move_in_invoice_type',
+        'account.action_move_in_refund_type',
+        'account.action_move_in_receipt_type',
+        'account.action_move_out_invoice_type',
+        'account.action_move_out_refund_type',
+        'account.action_move_out_receipt_type',
+    ]
+
+    for xml_id in actions_to_restore:
+        action = env.ref(xml_id, raise_if_not_found=False)
+        if action:
+            action.write({'view_mode': 'tree,kanban,form,activity'})
