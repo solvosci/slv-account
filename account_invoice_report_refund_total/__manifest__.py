6885a09730ecf24331790c23358286f3904fd2e4
David Alonso // Solvos <david.alonso@solvos.es>
2025-09-17 16:41:17 +0200

    [IMP] account_invoice_report_refund_total: all refund types coverage
    Before this improvement, only those refund invoices or bills that come
    from a regular invoice/bill were affected by "Total Refund" label
    renaming. If e.g. a refund invoice was created by other way, it was not
    applied, due to original and restricted requirements.
    
    After this improvement, the desired functionality is applied over every
    refund document, no matter if there is a reversal document is linked to
    it or not.

--- account_invoice_report_refund_total/__manifest__.py
+++ account_invoice_report_refund_total/__manifest__.py
@@ -7,7 +7,7 @@
     """,
     "author": "Solvos",
     "license": "LGPL-3",
-    "version": "17.0.1.0.1",
+    "version": "17.0.2.0.0",
     'category': "Accounting & Finance",
     "website": "https://github.com/solvosci/slv-account",
     "depends": ["account"],
--- account_invoice_report_refund_total/readme/CONTRIBUTORS.md
+++ account_invoice_report_refund_total/readme/CONTRIBUTORS.md
@@ -1,1 +1,2 @@
 * Iria Alonso \<iria.alonso@solvos.es\>
+* David Alonso \<david.alonso@solvos.es\>
