e60af6af18ac17329373e6728ded95db11ed87c9
Iria Alonso <iria.alonso@solvos.es>
2026-01-26 15:31:05 +0100

    [MIG] account_financial_report_menu: Migration to 17.0

--- account_financial_report_menu/__manifest__.py
+++ account_financial_report_menu/__manifest__.py
@@ -1,13 +1,13 @@
 # © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
-# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Account Financial Report Menu",
     "summary": """
         Change the menu position "Invoicing/Reports/OCA accounting reports" to "Invoicing/OCA accounting reports"
     """,
     "author": "Solvos",
-    "license": "LGPL-3",
-    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "version": "17.0.1.0.0",
     "category": "Account",
     "website": "https://github.com/solvosci/slv-account",
     "depends": [
--- account_financial_report_menu/hooks.py
+++ account_financial_report_menu/hooks.py
@@ -1,9 +1,8 @@
 # © 2024 Solvos Consultoría Informática (<http://www.solvos.es>)
-# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import SUPERUSER_ID, api
 
 
-def uninstall_hook(cr, registry, vals=None):
-    env = api.Environment(cr, SUPERUSER_ID, {})
+def uninstall_hook(env):
     env.ref("account_financial_report.menu_oca_reports").parent_id = env.ref("account.menu_finance_reports").id
