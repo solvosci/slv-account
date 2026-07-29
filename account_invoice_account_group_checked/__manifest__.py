ec2cfd3dfe84d905182a4376d688c2210f9c7bf4
David Alonso // Solvos <david.alonso@solvos.es>
2026-07-29 09:13:14 +0200

    [ADD] account_invoice_account_group_checked
    Sometimes, knowing that an invoice has lines with accounts belonging to
    certain account groups is needed.
    
    This addon achieves it in three steps:
    * For desired account group, set "Will Check Invoice" flag.
    * At this moment, every current and future invoice at draft state that have
      at least one account involved for this group, will be marked ("Has Account
      Checked Group" flag).
    * Invoices that have this flag (or not) can be easily located using filters
      currently available at invoices search views.
    
    When an account group is set, those invoices that are not at draft state won't
    be changed, in order to preserve invoice integrity and system overload. If we
    want to be checked, it must be achived outside this application.

---
+++ account_invoice_account_group_checked/__manifest__.py
@@ -0,0 +1,18 @@
+# © 2026 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Account Invoice Account Group Checked",
+    "version": "13.0.1.0.0",
+    "category": "Accounting/Accounting",
+    "summary": "Marks invoices when there are certain accounts involved, depending on their account group",
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "category": "Accounting & Finance",
+    "website": "https://github.com/solvosci/slv-account",
+    "depends": ["account"],
+    "data": [
+        "views/account_group_views.xml",
+        "views/account_move_views.xml",
+    ],
+    "installable": True,
+}
